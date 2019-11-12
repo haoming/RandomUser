8acc5f5c3df91ef14e9bad1ae9ae8a5c27ff14f9
Haoming Ma <haoming.ma@hotmail.com>
2019-11-12 21:28:46 +1300

    Initial Commit

---
+++ RandomUser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomUser
+//
+//  Created by etarip on 12/11/19.
+//  Copyright © 2019 Haoming. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
